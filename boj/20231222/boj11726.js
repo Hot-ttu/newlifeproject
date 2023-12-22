ea4987761d0723e947a010e424efa56e6781df8b
Hot-ttu <ghkdwndud7@naver.com>
2023-12-22 12:39:54 +0900

    [solved] boj11726.js

---
+++ boj/20231222/boj11726.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim();
+
+const N = +input;
+
+memo = [0, 1, 2];
+
+for (let i = 3; i < N + 1; i++) {
+  memo[i] = (memo[i - 1] + memo[i - 2]) % 10007;
+}
+// 매 값이 답이므로 매 값이 10007로 나눈 값이 들어가야 한다.
+
+console.log(memo[N]);
--- boj/main.js
+++ boj/main.js
@@ -10,4 +10,11 @@ const input = fs
 
 const N = +input;
 
-console.log(N);
+memo = [0, 1, 2];
+
+for (let i = 3; i < N + 1; i++) {
+  memo[i] = (memo[i - 1] + memo[i - 2]) % 10007;
+}
+// 매 값이 답이므로 매 값이 10007로 나눈 값이 들어가야 한다.
+
+console.log(memo[N]);
