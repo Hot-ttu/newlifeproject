7d0a9158428824d3809131d9717e77574995912a
Hot-ttu <ghkdwndud7@naver.com>
2023-12-25 21:13:50 +0900

    [solved] boj01927.js

---
+++ boj/20231225/boj01927.js
@@ -0,0 +1,93 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const T = +input.shift();
+
+const command = input.map(Number);
+
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  getLeftChildIdx = (parentIdx) => parentIdx * 2 + 1;
+  getRightChildIdx = (parentIdx) => parentIdx * 2 + 2;
+  getParentIdx = (childIdx) => Math.floor((childIdx - 1) / 2);
+
+  size = () => this.heap.length;
+
+  swap = (idx1, idx2) => {
+    [this.heap[idx1], this.heap[idx2]] = [this.heap[idx2], this.heap[idx1]];
+  };
+
+  peek = () => this.heap[0];
+
+  add = (num) => {
+    this.heap.push(num);
+    this.bubbleUp();
+  };
+
+  bubbleUp = () => {
+    let idx = this.heap.length - 1;
+    let parentIdx = this.getParentIdx(idx);
+
+    while (this.heap[parentIdx] && this.heap[idx] < this.heap[parentIdx]) {
+      this.swap(idx, parentIdx);
+      idx = parentIdx;
+      parentIdx = this.getParentIdx(idx);
+    }
+  };
+
+  poll = () => {
+    if (this.size() <= 0) {
+      return 0;
+    }
+
+    if (this.size() === 1) {
+      return this.heap.pop();
+    }
+
+    const num = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this.bubbleDown();
+    return num;
+  };
+
+  bubbleDown = () => {
+    let idx = 0;
+    let leftChildIdx = this.getLeftChildIdx(idx);
+    let rightChildIdx = this.getRightChildIdx(idx);
+
+    while (
+      (this.heap[leftChildIdx] && this.heap[leftChildIdx] < this.heap[idx]) ||
+      (this.heap[rightChildIdx] && this.heap[rightChildIdx] < this.heap[idx])
+    ) {
+      let smallerIdx = leftChildIdx;
+      if (this.heap[rightChildIdx] && this.heap[rightChildIdx] < this.heap[smallerIdx]) {
+        smallerIdx = rightChildIdx;
+      }
+
+      this.swap(idx, smallerIdx);
+      idx = smallerIdx;
+      leftChildIdx = this.getLeftChildIdx(idx);
+      rightChildIdx = this.getRightChildIdx(idx);
+    }
+  };
+}
+
+const minHeap = new MinHeap();
+const result = [];
+
+command.forEach((com) => {
+  if (com === 0) {
+    result.push(minHeap.poll());
+  } else {
+    minHeap.add(com);
+  }
+});
+
+console.log(result.join("\n"));
