a406e25aa44e7447a85d20fa5db6d4c25b63c0bf
Hot-ttu <ghkdwndud7@naver.com>
2024-01-11 08:35:15 +0900

    Solve: boj15652.js

---
+++ boj/2024/01/20240111/boj15652.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const [N, M] = input.shift().split(" ").map(Number);
+
+const result = [];
+let perm = [];
+const Perm = (idx, depth) => {
+  if (depth === M) {
+    console.log(perm);
+    result.push([...perm]);
+    return;
+  }
+
+  for (let i = idx; i <= N; i++) {
+    perm.push(i);
+    Perm(i, depth + 1);
+    perm.pop();
+  }
+};
+
+Perm(1, 0);
+console.log(result.map((line) => line.join(" ")).join("\n"));
